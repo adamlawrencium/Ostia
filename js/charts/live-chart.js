395fe23cd116a9cd91e68a7f245bebce5f4ccb24
Adam Lawrence <adam.ad.lawrence@gmail.com>
2016-10-19 17:13:40 -0400

    It's alive! Chart now dynamically updates

--- js/charts/live-chart.js
+++ js/charts/live-chart.js
@@ -1,18 +1,17 @@
 $( document ).ready(function() {
   $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
 
-    var socket = io.connect('http://localhost:3000');
 
 
     // When a stock update is received it's inserted in the page
     socket.on('message', function(data) {
-        //insertStock(data.message);
-        updateChart(data.message);
+      //insertStock(data.message);
+      updateChart(data.message);
     })
 
     // Adds a stock update to the page
     function insertStock(message) {
-    //    $('#stock_zone').prepend('</strong> ' + message + '</p>');
+      //    $('#stock_zone').prepend('</strong> ' + message + '</p>');
     }
 
     function updateChart(message) {
@@ -33,21 +32,21 @@ $( document ).ready(function() {
     $('#container').highcharts('StockChart', {
 
       rangeSelector: {
-           buttons: [{
-               count: 1,
-               type: 'minute',
-               text: '1M'
-           }, {
-               count: 5,
-               type: 'minute',
-               text: '5M'
-           }, {
-               type: 'all',
-               text: 'All'
-           }],
-           inputEnabled: false,
-           selected: 0
-       },
+        buttons: [{
+          count: 1,
+          type: 'minute',
+          text: '1M'
+        }, {
+          count: 5,
+          type: 'minute',
+          text: '5M'
+        }, {
+          type: 'all',
+          text: 'All'
+        }],
+        inputEnabled: false,
+        selected: 0
+      },
 
       title: {
         text: 'fasdfas Stock Price'
@@ -56,11 +55,31 @@ $( document ).ready(function() {
         type: 'spline',
         events: {
           load: function() {
+
             var self = this;
             var n = 0;
+            var socket = io.connect('http://localhost:3000');
+
             socket.on('message', function(data) {
-              console.log([data.message[0],data.message[1]]);
-              self.series.addPoint([data.message[0],data.message[1]]);
+              //console.log([data.message[0],data.message[1]]);
+
+              var tim = (new Date()).getTime()
+              var y   = Math.random();
+              self.series[0].addPoint([tim, y]);
+
+              var time    = data.message[0];
+              var highbid = data.message[1];
+              var lowask  = data.message[2];
+              //self.redraw();
+              // for (var entry in self.series.data) {
+              //   if (self.series[0].hasOwnProperty(entry)) {
+              //     console.log(entry);
+              //   }
+              // }
+
+              //console.log(self.series[0].setData([time,lowask]));
+
+              //self.series.addPoint([data.message[0],data.message[1]]);
               //self.series[0].setData([data.message[0],n]);
               n = n * (1.01);
             })
@@ -80,23 +99,23 @@ $( document ).ready(function() {
         name: 'BANANA',
         data: [],
         /*data: [
-          [1475452800000,112.52],
-          [1475539200000,113.00],
-          [1475625600000,113.05],
-          [1475712000000,113.89],
-          [1475798400000,114.06],
-          [1476057600000,116.05],
-          [1476144000000,116.30],
-          [1476230400000,117.34],
-          [1476316800000,116.98],
-          [1476403200000,117.63],
-          [1476662400000,117.55],
-          [1476748800000,117.47]
-        ],*/
-          tooltip: {
-            valueDecimals: 2
-        }
-      }]
-    });
+        [1475452800000,112.52],
+        [1475539200000,113.00],
+        [1475625600000,113.05],
+        [1475712000000,113.89],
+        [1475798400000,114.06],
+        [1476057600000,116.05],
+        [1476144000000,116.30],
+        [1476230400000,117.34],
+        [1476316800000,116.98],
+        [1476403200000,117.63],
+        [1476662400000,117.55],
+        [1476748800000,117.47]
+      ],*/
+      tooltip: {
+        valueDecimals: 2
+      }
+    }]
   });
+});
 });
