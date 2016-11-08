844f9aa757503be06be58cddc053adfd3814ee9b
Adam Lawrence <adam.ad.lawrence@gmail.com>
2016-11-08 00:57:32 -0500

    Glued together files: server->TradingDesk->arbitrage

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* WELCOME TO THE OSTIA TRADING PLATFORM */
+
 var express = require('express');
 var app     = express();
 
@@ -5,29 +7,17 @@ var server  = require('http').Server(app);
 var io      = require('socket.io')(server);
 var path    = require('path');
 
-// Beginning arbitrage
-var mapParse        = require("./lib/exchanges/map-parsing.js");
-var arbitrage       = require("./lib/strategies/simple-arbitrage.js").largestSpreadArbitrage;
-var Order           = require("./lib/strategies/simple-arbitrage.js").order;
-var allExchangeData = require("./lib/DataHub.js");
-
-
-arbitrage("BTCUSD", 1000);
-console.log("Before Order");
-console.log(Order);
-console.log("After Order");
-//arbitrage("ETHBTC", 1000);
-
-// Set up Trading desk and run strategy
-/*var TradingDesk = require("./lib/TradingDesk.js");*/
 
 // TODO: Determine which data to send to the client for viz's
-// Live trading (performance) data that will be sent to client via Socket.IO
-//var TDInfo = TradingDesk.runLiveTrading;
+//Live trading (performance) data that will be sent to client via Socket.IO
+
+// Set up Trading desk and run strategy
+var runLiveTrading = require("./lib/TradingDesk.js");
+runLiveTrading();
+
 
 // Created to start and stop the liveFeed of a exchange
 var liveFeed;
-
 
 // This sends data to the client for visualizations with socket.io
 // Handling the connection to the client through socket.io
@@ -36,7 +26,10 @@ io.sockets.on('connection', function (socket) {
     // Creating a live feed to the client of the data requested
     liveFeed = setInterval(function() {
       var date = new Date();
-      socket.emit('message', {message: [Order, date.getTime(), mapParse(allExchangeData[data.data], data.data)]})
+      socket.emit('message', { message: [Order,
+                                        date.getTime(),
+                                        mapParse(allExchangeData[data.data], data.data)]
+                                      })
     }, 1000)
   });
   socket.on('closeExchange', function(data) {
@@ -54,7 +47,6 @@ io.sockets.on('connection', function (socket) {
 //************************/
 
 app.use(express.static('client'));
-
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/client/html/index.html');
