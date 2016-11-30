a48094b1a8dcb79ff63fd7b3ee1220600f4f1ebe
Ming Luo <mcluo1994@gmail.com>
2016-11-30 05:26:04 +0000

    Applied linter rules

--- client/js/sockets.js
+++ client/js/sockets.js
@@ -2,11 +2,11 @@
 // TODO: Create some sort of updating/reset function for highcharts,
 //        currently the data changes, but the chart won't reset
 // TODO: Determine which data should be sent to the client for visualizations
-var socket = io.connect('http://localhost:3000');
-  function Graph(data){
-    var exchange = data;
+var socket = io.connect('http://localhost:3000')
+function Graph (data) {
+  var exchange = data
 
     // Closing the previous exchange feed and starting the new one
-    socket.emit('closeExchange', {data: null});
-    socket.emit('openExchange', {data: exchange});
-  }
+  socket.emit('closeExchange', {data: null})
+  socket.emit('openExchange', {data: exchange})
+}
