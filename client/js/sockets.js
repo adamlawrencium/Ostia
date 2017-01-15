fc8ab5beafb679be35ffd0251bd2efba9d9a2492
Adam Lawrence <adam.ad.lawrence@gmail.com>
2017-01-15 16:20:44 -0500

    tri arb integration

--- client/js/sockets.js
+++ client/js/sockets.js
@@ -6,7 +6,7 @@ var socket = io.connect('http://localhost:3000')
 function Graph (data) {
   var exchange = data
 
-    // Closing the previous exchange feed and starting the new one
+  // Closing the previous exchange feed and starting the new one
   socket.emit('closeExchange', {data: null})
   socket.emit('openExchange', {data: exchange})
 }
