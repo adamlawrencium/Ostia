11e0c9f8354c7fa3abf6f6eca7de85ee76cb9ce7
adamadlawrence <adam.ad.lawrence@gmail.com>
2017-02-24 21:49:09 -0500

    Implemented moving average crossover logic
    Fix data routing

--- server.js
+++ server.js
@@ -37,15 +37,17 @@ io.sockets.on('connection', function (socket) {
       var financialData = require('./lib/strategies/basicStrategy.js');
       var candlestickData = financialData.candlestickData;
       var indicators = financialData.indicators;
-
-      callback(null, candlestickData, indicators, financialData);
+      var flags = financialData.flags;
+      callback(null, candlestickData, indicators, financialData, flags);
     },
 
     /* emit initialized data */
-    function (candlestickData, indicators, financialData, callback) {
+    function (candlestickData, indicators, financialData, flags, callback) {
+      console.log(orders);
       socket.emit('initializedChartData', {
         candlestickData: candlestickData,
-        indicators: indicators
+        indicators: indicators,
+        flags: flags
       });
       socket.emit('chartFlags', {
         flags: null
@@ -65,7 +67,7 @@ io.sockets.on('connection', function (socket) {
         console.log("### SERVER: Live Data Point Sent")
       }, 5000);
     }
-    
+
   ], function (err, result) {
     // result now equals 'done'
   });
